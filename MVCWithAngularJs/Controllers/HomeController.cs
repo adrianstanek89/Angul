1a467bfadd8b635cb029c7b9a52491f1e96b1b72
adrianstanek89 <31177062+adrianstanek89@users.noreply.github.com>
2018-02-07 11:35:30 +0100

    Add files via upload

---
+++ MVCWithAngularJs/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWithAngularJs.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Part2() 
+        {
+            return View();
+        }
+
+        public ActionResult Part3() 
+        {
+            return View();
+        }
+
+        public ActionResult Part4() 
+        {
+            return View();
+        }
+
+        public ActionResult Part5() 
+        {
+            return View();
+        }
+
+        public ActionResult Part6() 
+        {
+            return View();
+        }
+
+    }
+}
